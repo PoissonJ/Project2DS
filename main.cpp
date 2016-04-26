19c2ca5fb4224cfcd0b418ee3d121c4e05e46c46
Kenan Yildirim <kenan@kenany.me>
2016-04-26 11:27:44 -0400

    enforce consistent style

--- main.cpp
+++ main.cpp
@@ -34,30 +34,34 @@ static unsigned int leven(string a, string b) {
 
   // Fill table bottom up
   for (unsigned int i = 0; i <= aLength; i++) {
-      for (unsigned int j = 0; j <= bLength; j++) {
+    for (unsigned int j = 0; j <= bLength; j++) {
 
-          // First string empty, insert all of second string
-          if (i == 0)
-              subTable[i][j] = j;  // Minimum operations = j
-
-          // Second string empty, insert all of first string
-          else if (j == 0)
-              subTable[i][j] = i;  // Minimum operations = i
-
-          // If last characters are the same, ignore last character and
-          // continue for remaining string
-          else if (a[i-1] == b[j-1])
-              subTable[i][j] = subTable[i-1][j-1];
-
-          // If last characters are not the same, find minimum from all
-          // possibilities
-          else
-              subTable[i][j] = 1 + min(min(
-                                            subTable[i-1][j-1], // Replace
-                                            subTable[i][j-1]),  // Insert
-                                            subTable[i-1][j]    // Remove
-                                           );
+      // First string empty, insert all of second string
+      if (i == 0) {
+        subTable[i][j] = j;  // Minimum operations = j
       }
+
+      // Second string empty, insert all of first string
+      else if (j == 0) {
+        subTable[i][j] = i;  // Minimum operations = i
+      }
+
+      // If last characters are the same, ignore last character and
+      // continue for remaining string
+      else if (a[i-1] == b[j-1]) {
+        subTable[i][j] = subTable[i-1][j-1];
+      }
+
+      // If last characters are not the same, find minimum from all
+      // possibilities
+      else {
+        subTable[i][j] = 1 + min(min(
+          subTable[i-1][j-1], // Replace
+          subTable[i][j-1]),  // Insert
+          subTable[i-1][j]    // Remove
+         );
+      }
+    }
   }
 
   return subTable[aLength][bLength];
@@ -75,20 +79,22 @@ static vector<unsigned int> maxMagi( vector<unsigned int> a){
   unsigned int maxLIS = 0;
 
   // initialize array with 1s
-  for(unsigned int i=0; i<n; i++)
+  for(unsigned int i = 0; i < n; i++) {
     lis[i] = 1;
+  }
 
   // calculate LIS values in dynamic method
-  for(unsigned int i=1; i<n; i++){
-    for(unsigned int j=0; j<i; j++){
-      if(a[i] > a[j] && lis[i] < lis[j]+1)
-        lis[i] = lis[j]+1;
+  for (unsigned int i = 1; i < n; i++) {
+    for (unsigned int j = 0; j < i; j++) {
+      if (a[i] > a[j] && lis[i] < lis[j] + 1) {
+        lis[i] = lis[j] + 1;
+      }
     }
   }
 
   // find maxLIS
-  for(unsigned int i=0; i<n; i++){
-    if(maxLIS < lis[i]){
+  for (unsigned int i = 0; i < n; i++) {
+    if (maxLIS < lis[i]) {
       maxLIS = lis[i];
       returnVector.push_back(a[i]);
     }
